8f2cb21c944f608b36a0b81faf0c9ad006b4834e
Akshayalakshmi S <crystalfiresword@gmail.com>
2025-11-18 21:16:20 +0530

    Change quality update implementation for aged brie

--- python/items/aged_brie.py
+++ python/items/aged_brie.py
@@ -1,6 +1,7 @@
 def update_quality_aged_brie(item, max_aged_brie_quality):
-    item.sell_in-=1
-    item.quality=min(item.quality+1, max_aged_brie_quality)   
+    item.sell_in-=1  
     if item.sell_in<0:     
-        item.quality=min(item.quality+1, max_aged_brie_quality)   
+        item.quality=min(item.quality+2, max_aged_brie_quality)   
+    else:
+        item.quality=min(item.quality+1, max_aged_brie_quality) 
     return
