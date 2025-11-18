801150316e57b4490ccc497a3d4ebd31ad0bca85
Akshayalakshmi S <crystalfiresword@gmail.com>
2025-11-18 21:16:36 +0530

    Change quality update implementation for backstage

--- python/items/backstage.py
+++ python/items/backstage.py
@@ -5,20 +5,18 @@ def update_quality_backstage(item, max_backstage_quality):
     """        
     CONCERT_DAYS_UB_1 = 10   
     CONCERT_DAYS_UB_2 = 5  
+    CONCERT_DAYS_LB = 0
     
     # decrement number of days left to sell
     item.sell_in-=1
     
-    if item.sell_in<0:
-        item.quality=0
-        return
-    
-    item.quality=min(item.quality+1, max_backstage_quality) 
-
-    if item.sell_in<CONCERT_DAYS_UB_1:
+    if item.sell_in>=CONCERT_DAYS_UB_1:
         item.quality=min(item.quality+1, max_backstage_quality) 
-
-    if item.sell_in<CONCERT_DAYS_UB_2:
-        item.quality=min(item.quality+1, max_backstage_quality)
+    elif item.sell_in>=CONCERT_DAYS_UB_2:
+        item.quality=min(item.quality+2, max_backstage_quality) 
+    elif item.sell_in>=CONCERT_DAYS_LB:
+        item.quality=min(item.quality+3, max_backstage_quality)
+    else:
+        item.quality=0       
     
     return 
