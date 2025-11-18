68517f75eabd1b02309f8d5a4bd2893d09059877
Akshayalakshmi S <crystalfiresword@gmail.com>
2025-11-18 17:57:13 +0530

    Add comments and remove checking function

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-    MAX_QUALITY = 50
-    MIN_QUALITY = 0
-
+    """
+    Quality-related class variables
+    """
+    _MAX_ITEM_QUALITY = 50 
+    _MIN_ITEM_QUALITY = 0
+    _MAX_AGED_BRIE_QUALITY = _MAX_ITEM_QUALITY
+    _MIN_AGED_BRIE_QUALITY = _MIN_ITEM_QUALITY
+    _MAX_CONJURED_QUALITY = _MAX_ITEM_QUALITY
+    _MIN_CONJURED_QUALITY = _MIN_ITEM_QUALITY
+    _SULFURAS_MAX_ITEM_QUALITY = 80    
+    
     def __init__(self, items):
         self.items = items
         self.update_quality_special_products = {
             "Aged Brie":self._update_quality_Aged_Brie, 
-            "Backstage passes to a TAFKAL80ETC concert": self._update_quality_backstage, 
+            "Conjured Mana Cake":self._update_quality_conjured,
             "Sulfuras, Hand of Ragnaros": self._update_quality_Sulfuras, 
-            "Conjured Mana Cake":self._update_quality_conjured
+            "Backstage passes to a TAFKAL80ETC concert": self._update_quality_backstage, 
             }
         
     def update_quality(self):
@@ -23,49 +31,53 @@ class GildedRose(object):
             
             item.sell_in-=1
              
-            item.quality=max(item.quality-1, GildedRose.MIN_QUALITY)
+            item.quality=max(item.quality-1, GildedRose._MIN_ITEM_QUALITY)
             if item.sell_in<0:
-                item.quality=max(item.quality-1, GildedRose.MIN_QUALITY)
-
-    def check_quality_limit(self, item, upper_limit=MAX_QUALITY):
-        if item.quality>upper_limit or item.quality<0:
-            return False
-        return True
+                item.quality=max(item.quality-1, GildedRose._MIN_ITEM_QUALITY)
     
-    def _update_quality_Aged_Brie(self, item): 
+    def _update_quality_Aged_Brie(self, item):
         item.sell_in-=1
-        item.quality=min(item.quality+1, GildedRose.MAX_QUALITY)   
+        item.quality=min(item.quality+1, GildedRose._MAX_AGED_BRIE_QUALITY)   
         if item.sell_in<0:     
-            item.quality=min(item.quality+1, GildedRose.MAX_QUALITY)   
+            item.quality=min(item.quality+1, GildedRose._MAX_AGED_BRIE_QUALITY)   
         return
         
     def _update_quality_backstage(self, item):
+
+        """
+        bounds on the number of days remaining to sell concert tickets, 
+        based on which quality is modified differently
+        """
+        
+        CONCERT_DAYS_UB_1 = 10   
+        CONCERT_DAYS_UB_2 = 5  
+        
         # decrement number of days left to sell
         item.sell_in-=1
-
-        CONCERT_DAYS_UB_1 = 10
-        CONCERT_DAYS_UB_2 = 5
+        
         if item.sell_in<0:
             item.quality=0
             return
-        item.quality=max(item.quality-1, GildedRose.MIN_QUALITY)
+        
+        item.quality=min(item.quality+1, GildedRose._MAX_ITEM_QUALITY) 
+
         if item.sell_in<CONCERT_DAYS_UB_1:
-            item.quality=min(item.quality+1, GildedRose.MAX_QUALITY)        
+            item.quality=min(item.quality+1, GildedRose._MAX_ITEM_QUALITY) 
+
         if item.sell_in<CONCERT_DAYS_UB_2:
-            item.quality=min(item.quality+1, GildedRose.MAX_QUALITY)
+            item.quality=min(item.quality+1, GildedRose._MAX_ITEM_QUALITY)
         
         return 
        
     def _update_quality_Sulfuras(self, item):
-        SULFURAS_MAX_QUALITY = 80
-        item.quality=SULFURAS_MAX_QUALITY
+        item.quality=GildedRose._SULFURAS_MAX_ITEM_QUALITY
         return
     
     def _update_quality_conjured(self, item):
         item.sell_in-=1                  
-        item.quality=max(item.quality-2, GildedRose.MIN_QUALITY)
+        item.quality=max(item.quality-2, GildedRose._MIN_CONJURED_QUALITY)
         if item.sell_in<0:
-            item.quality=max(item.quality-2, GildedRose.MIN_QUALITY)
+            item.quality=max(item.quality-2, GildedRose._MIN_CONJURED_QUALITY)
         return
     
 class Item:
@@ -76,20 +88,3 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
-'''
-            Conditions:
-            1. quality always >0 and <=50
-            2. sell_in can be negative
-            3. assume when sell_in>0 and default, lower both of them by 1
-            4. if sell_in<0: degrade quality twice;
-            5. Anomaly:
-                i. Aged Brie - increase quality, but always <=50
-                ii. Sulfuras - do nothing, set maximum quality 80
-                iii. Backstage pass - increase Q by 2 if 5<sellIn<10, and by 3 if 0<sellIn<5, 0 otherwise
-                iv. Conjured - degrade twice as fast in quality
-            Steps:
-            1. Have a dictionary that has a dict for special products, and each product have its own update function
-            2. Otherwise, do a normal decrement
-            3. Define class variables for magic numbers
-            '''
