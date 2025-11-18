c27e59cf8ea98837fe54f244c8ca3f8641ee09f0
Akshayalakshmi S <crystalfiresword@gmail.com>
2025-11-18 21:08:55 +0530

    Modify path for import in testing

--- python/tests/test_gilded_rose_approvals.py
+++ python/tests/test_gilded_rose_approvals.py
@@ -1,5 +1,11 @@
 import io
 import sys
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir) 
+sys.path.append(project_root)
 
 from approvaltests import verify
 from texttest_fixture import main
